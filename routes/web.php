00e2be0c14f4c51ddf234a8cabc0b8f7b0a29ca0
ngDuyHung <nguyenduyhung2456@gmail.com>
2025-12-06 23:26:37 +0700

    update new create folder module and auth

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', function () {
-    return view('admin');
-});
+Route::get('/dashboard', function () {
+    return view('admin.home');
+})->name('dashboard');
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
